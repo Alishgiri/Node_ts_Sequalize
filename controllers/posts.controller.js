5bc6c6f0b3770215749e85b57e3ed8f9edcef7a1
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-03-12 16:47:52 +0545

    Fix bugs in posts contoller

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,10 +1,24 @@
 const models = require('../database/models');
 
+module.exports.getAllPosts = async (req, res) => {
+    try {
+        const posts = await models.Post.findAll({
+            include: [
+                // { model: models.Comment, as: 'comments' },
+                { model: models.User, as: 'author' }
+            ]
+        });
+        return res.send(posts)
+    } catch (e) {
+        return res.status(500).send({ error: e.message });
+    }
+}
+
 module.exports.createPost = async (req, res) => {
     try {
         const post = await models.Post.create(req.body);
         return res.status(201).json({ post });
     } catch (e) {
-        return res.status(500).json({ error: error.message });
+        return res.status(500).json({ error: e.message });
     }
 }
